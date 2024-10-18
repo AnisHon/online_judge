ec8bef9743faeab57a8e445d039e48a260c64ba2
AnisHan <88761857+AnisHon@users.noreply.github.com>
2024-10-18 14:27:46 +0800

    实现文件夹 实现选择题 填空题判题

---
+++ problem-service/src/main/java/com/anishan/problem/controller/ListController.java
@@ -0,0 +1,72 @@
+package com.anishan.problem.controller;
+
+import com.anishan.commons.e.ValidationGroup;
+import com.anishan.commons.entity.R;
+import com.anishan.problem.domain.dto.ProblemListDto;
+import com.anishan.problem.domain.dto.ProblemListRelationDto;
+import com.anishan.problem.mapper.ProblemProblemListMapper;
+import com.anishan.problem.service.ProblemListService;
+import com.anishan.problem.service.ProblemProblemListService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/list")
+@RequiredArgsConstructor(onConstructor_ = {@Autowired})
+@Api("题单接口")
+public class ListController {
+
+    private final ProblemListService problemListService;
+    private final ProblemProblemListService problemProblemListService;
+
+    @PostMapping("/add")
+    @PreAuthorize("hasAuthority('problem:list:add')")
+    @ApiOperation("添加题单")
+    public R<Boolean> add(@RequestBody @Validated(ValidationGroup.Insert.class) ProblemListDto problemListDto) {
+        boolean b = problemListService.addProblemList(problemListDto);
+        return R.success(b);
+    }
+
+    @GetMapping("/del/{ids}")
+    @PreAuthorize("hasAuthority('problem:list:delete')")
+    @ApiOperation("删除题单 使用：/del/1,2")
+    public R<String> delete(@PathVariable("ids")List<Long> ids) {
+        problemListService.delProblemList(ids);
+        return R.success();
+    }
+
+    @PostMapping("/update")
+    @PreAuthorize("hasAuthority('problem:list:update')")
+    @ApiOperation("修改题单")
+    public R<Boolean> update(@RequestBody @Validated(ValidationGroup.Update.class) ProblemListDto problemListDto) {
+        boolean b = problemListService.updateProblem(problemListDto);
+        return R.success(b);
+    }
+
+    @PostMapping("/add-problem")
+    @PreAuthorize("hasAuthority('problem:list:add-problem')")
+    @ApiOperation("为题单添加题目")
+    public R<Boolean> addProblem(@RequestBody List<ProblemListRelationDto> relations) {
+        boolean b = problemListService.addProblem(relations);
+        return R.success(b);
+    }
+
+    @PostMapping("/del-problem")
+    @PreAuthorize("hasAuthority('problem:list:del-problem')")
+    @ApiOperation("为题单删除题目")
+    public R<Boolean> delProblem(@RequestBody @Validated(ValidationGroup.Delete.class) ProblemListRelationDto relations) {
+        boolean b = problemListService.delProblem(relations);
+        return R.success(b);
+    }
+
+
+
+}
