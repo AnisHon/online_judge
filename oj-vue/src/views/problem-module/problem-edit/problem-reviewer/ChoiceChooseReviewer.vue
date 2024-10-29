4f0cb1665dace0df814705f7c1aea6250b68cf29
AnisHan <3137687133@qq.com>
2024-10-29 16:35:47 +0800

    题目编辑

---
+++ oj-vue/src/views/problem-module/problem-edit/problem-reviewer/ChoiceChooseReviewer.vue
@@ -0,0 +1,35 @@
+
+<template>
+
+  <div>
+    <div class="choices">
+      <ProblemRadioGroup ref="problemRadioGroupRef" :choices="choices" :is-multi="isMulti" />
+    </div>
+  </div>
+
+</template>
+
+<script setup lang="ts">
+import {type ProblemForm, ProblemType} from "@/api/problem";
+import {computed, ref} from "vue";
+import ProblemRadioGroup from "@/views/problem-module/problem-edit/radio-group-viewer/index.vue";
+
+const {problemView} = defineProps<{
+  problemView: ProblemForm,
+}>();
+const problemRadioGroupRef = ref<InstanceType<typeof ProblemRadioGroup> | null>(null);
+
+const choices = computed(() => {
+  return problemView?.choices || []
+})
+
+const isMulti = computed(() => {
+  return problemView.problem.type === ProblemType.MULTI_CHOICE
+})
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
